b04e62bd6f40073d9ec92df31afaed75e12250e1
BS <bs@bss-mac-pro.home>
2020-06-10 11:13:34 +0200

    launch the project

---
+++ my-website/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+	<div id="app">
+		<img alt="Vue logo" src="./assets/logo.png" />
+		<HelloWorld :msg="message" para1="This is my first paragraph" />
+		<div>
+			<input type="text" v-model="message" />
+			<button @click="handelClick" type="submit">Send</button>
+		</div>
+	</div>
+</template>
+
+<script>
+	import HelloWorld from "./components/HelloWorld.vue";
+
+	export default {
+		name: "App",
+		components: {
+			HelloWorld,
+		},
+		data() {
+			return {
+				message: "This is my main message",
+			};
+		},
+		methods: {
+			handelClick() {
+				console.log(`Your message: ${this.message}`);
+			},
+		},
+	};
+</script>
+
+<style>
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
+</style>
